8ef449b5ced7652787b53d3774e5cd9e6b589021
vladislav_volynets <flatislove@gmail.com>
2026-09-11 16:25:35 +0500

    db cache fix

--- database/db.py
+++ database/db.py
@@ -12,7 +12,12 @@ if DATABASE_URL.startswith("postgres://"):
 elif DATABASE_URL.startswith("postgresql://"):
     DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=False, 
+    connect_args={"statement_cache_size": 0}
+)
+
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 class Base(DeclarativeBase):
